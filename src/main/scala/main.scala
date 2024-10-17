7f9fd954803ca13311f30df632379cf558e18299
Pascal-2 <pascal.kaimer@gmail.com>
2024-10-17 15:53:04 +0200

    Übernahme beider Mergekonfliktkommentare

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -9,7 +9,10 @@
       for (j <- 0 to 3) {
         printf(" " * 3 + i + " " + j + " " * 4)
       }
+
       printf("\n" * 3) //ich, Pascal, sage dies sei das letzte printf in main!
+      // Alex möchte Konflikt vermeiden, aber für die Aufgabe muss es sein. :(
+
     }
   }
   // als Funktion/Methode
