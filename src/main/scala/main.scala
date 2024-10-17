64c01e417ae0e677a9aca330a204248cbd9b2066
Pascal-2 <pascal.kaimer@gmail.com>
2024-10-17 15:42:56 +0200

    Kommentar für Merge_Konflikt in main

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -9,7 +9,7 @@
       for (j <- 0 to 3) {
         printf(" " * 3 + i + " " + j + " " * 4)
       }
-      printf("\n" * 3)
+      printf("\n" * 3) //ich, Pascal, sage dies sei das letzte printf in main!
     }
   }
   // als Funktion/Methode
