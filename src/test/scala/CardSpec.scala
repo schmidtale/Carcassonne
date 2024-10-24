bbdf3c692b2ffdb92704c3be12619d3c964ff777
Pascal-2 <pascal.kaimer@gmail.com>
2024-10-24 15:51:40 +0200

    tightened the demands of the CardSpec test

--- src/test/scala/CardSpec.scala
+++ src/test/scala/CardSpec.scala
@@ -2,7 +2,10 @@ import org.scalatest.wordspec.AnyWordSpec
 import org.scalatest.matchers.should.Matchers._
 
 class CardSpec extends AnyWordSpec {
-  val card = new Card(borders = Array(BorderType.road, BorderType.pasture, BorderType.town, BorderType.pasture))
+  val card = new Card(borders = Array(BorderType.road, BorderType.town, BorderType.road, BorderType.pasture))
+  val card2 = new Card(borders = Array(BorderType.town, BorderType.pasture, BorderType.pasture, BorderType.town))
+  val card3 = new Card(false, borders = Array(BorderType.road, BorderType.road, BorderType.road, BorderType.pasture))
+  
   "A card" should {
     "return the type of its northern border" in {
       assert(card.borderType(Orientation.northern) == BorderType.road |
@@ -13,8 +16,11 @@ class CardSpec extends AnyWordSpec {
       assert(!card.monastery | card.monastery)
     }
     "return how its inner fields are connected" in {
-      assert(card.fieldConnections(Field.upperLeft).isInstanceOf[Array[Boolean]])
-      assert(card.fieldConnections(Field.upperLeft).length == 4)
+      assert(card.fieldConnections.isInstanceOf[Array[Boolean]])
+      assert(card.fieldConnections.length == 4)
+      assert(card.fieldConnections == Array(false, true, false, true))
+      assert(card2.fieldConnections == Array(true, true, true, true))
+      assert(card3.fieldConnections == Array(false, false, false, true))
     }
   }
 }
