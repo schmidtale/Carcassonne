950c81be530bf3c1859f9931412110572d50d49f
Pascal-2 <pascal.kaimer@gmail.com>
2024-11-07 14:29:28 +0100

    new class specifies how a TextProvider (to be implemented) should take a Card and generate line wise output for TUI printing

---
+++ src/test/scala/TextProviderSpec.scala
@@ -0,0 +1,20 @@
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatest.matchers.should.Matchers.*
+import BorderType._
+import LiegemanPosition.north
+import LiegemanType.knight
+
+class TextProviderSpec extends AnyWordSpec {
+  val card2 = new Card(false, true, borders = Array(town, pasture, pasture, town), (knight, north))
+
+  "A TextProvider" should {
+    "translate the data of a card to Strings used by the TUI" in {
+      assert(card2 line 0 == "B B B B B")
+      assert(card2 line 1 == "B B . . .")
+      assert(card2 line 2 == "B . . . .")
+      assert(card2 line 3 == "B . . . .")
+      assert(card2 line 4 == "B . . . .")
+    }
+  }
+
+}
