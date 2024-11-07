971c01ebbfe16942e5e1fa6d7141f52f3f18d836
Pascal-2 <pascal.kaimer@gmail.com>
2024-11-07 22:14:39 +0100

    partially working update of TextProvider: replaceChar can now place lots of town tiles ('B')

--- src/main/scala/TextProvider.scala
+++ src/main/scala/TextProvider.scala
@@ -13,13 +13,57 @@ class TextProvider {
     cardString
   }
 
-  def replaceChar(char: Char, c: Card): Char = {
+  private def replaceChar(char: Char, c: Card): Char = {
     (char, c.borders(0), c.borders(1), c.borders(2), c.borders(3), c.townConnection, c.monastery) match {
-      //case ('|', _, _, _) => '\n'
-      case ('a', town, _, _, _, _, _) => 'B'
-      case ('A', town, _, _, _, _, _) => 'B'
-      case ()
-      case other => other // Default to original character if no match
+      case ('|', _, _, _, _, _, _) => '\n'
+      
+      case ('a', BorderType.town, _, _, _, _, _) => 'B'
+      case ('a', BorderType.pasture, _, _, _, _, _) => '.'
+      case ('a', BorderType.road, _, _, _, _, _) => '.'
+      case ('A', BorderType.town, _, _, _, _, _) => 'B'
+      case ('A', BorderType.pasture, _, _, _, _, _) => '.'
+      case ('A', BorderType.road, _, _, _, _, _) => 'H'
+      
+      case ('b', _, BorderType.town, _, _, _, _) => 'B'
+      case ('b', _, BorderType.pasture, _, _, _, _) => '.'
+      case ('b', _, BorderType.road, _, _, _, _) => '.'
+      case ('B', _, BorderType.town, _, _, _, _) => 'B'
+      case ('B', _, BorderType.pasture, _, _, _, _) => '.'
+      case ('B', _, BorderType.road, _, _, _, _) => 'H'
+
+      case ('c', _, _, BorderType.town, _, _, _) => 'B'
+      case ('c', _, _, BorderType.pasture, _, _, _) => '.'
+      case ('c', _, _, BorderType.road, _, _, _) => '.'
+      case ('C', _, _, BorderType.town, _, _, _) => 'B'
+      case ('C', _, _, BorderType.pasture, _, _, _) => '.'
+      case ('C', _, _, BorderType.road, _, _, _) => 'H'
+
+      case ('d', _, _, _, BorderType.town, _, _) => 'B'
+      case ('d', _, _, _, BorderType.pasture, _, _) => '.'
+      case ('d', _, _, _, BorderType.road, _, _) => '.'
+      case ('D', _, _, _, BorderType.town, _, _) => 'B'
+      case ('D', _, _, _, BorderType.pasture, _, _) => '.'
+      case ('D', _, _, _, BorderType.road, _, _) => 'H'
+      
+      case ('e'|'f'|'g'|'h'|'i'|'j'|'k'|'l'|'M', 
+        BorderType.town, BorderType.town, BorderType.town, BorderType.town, _, _) => 'B'
+      
+      case ('i'|'f'|'j'|'g'|'k'|'M', BorderType.town, BorderType.town, BorderType.town, _, _, _) => 'B'
+      case ('l'|'e'|'i'|'f'|'j'|'M', BorderType.town, BorderType.town, _, BorderType.town, _, _) => 'B'
+      case ('k'|'h'|'l'|'e'|'i'|'M', BorderType.town, _, BorderType.town, BorderType.town, _, _) => 'B'
+      case ('j'|'g'|'k'|'h'|'l'|'M', _, BorderType.town, BorderType.town, BorderType.town, _, _) => 'B'
+
+      case ('e', _, BorderType.town, _, BorderType.town, true, _) => 'B'
+      case ('e', _, BorderType.town, _, BorderType.town, false, _) => '.'
+      case ('f', BorderType.town, BorderType.town, _, _, true, _) => 'B'
+      case ('f', BorderType.town, BorderType.town, _, _, false, _) => '.'
+      case ('g', _, BorderType.town, BorderType.town, _, true, _) => 'B'
+      case ('g', _, BorderType.town, BorderType.town, _, false, _) => '.'
+      case ('h', _, _, BorderType.town, BorderType.town, true, _) => 'B'
+      case ('h', _, _, BorderType.town, BorderType.town, false, _) => '.'
+      
+      
+      case other => '#'
     }
   }
 
