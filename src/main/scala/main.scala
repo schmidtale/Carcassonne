a21b6bb8b4ce85bb5ba54109ec9678d5a60b641f
Pascal-2 <pascal.kaimer@gmail.com>
2024-11-08 11:46:56 +0100

    changed main to call startTUI

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,10 +1,12 @@
 @main
 def main(): Unit = {
-  val tabletop = new Tabletop
-  print(tabletop.constructTabletop())
+//  val tabletop = new Tabletop
+//  print(tabletop.constructTabletop())
+//
+//  val emptyMap = tabletop.emptyMap()
+//  print(tabletop.constructTabletopFromMap(emptyMap))
 
-  val emptyMap = tabletop.emptyMap()
-  print(tabletop.constructTabletopFromMap(emptyMap))
+    startTUI()
 }
 
   
