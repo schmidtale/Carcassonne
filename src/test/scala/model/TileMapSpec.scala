c877acf09b4a3b4c1e9ec3d7459b00f04aebeb75
schmidtale <schmidt.trossingen@web.de>
2024-11-14 16:07:15 +0100

    Changed Card to Tile according to previous Refactor

--- src/test/scala/model/TileMapSpec.scala
+++ src/test/scala/model/TileMapSpec.scala
@@ -8,7 +8,7 @@ class TileMapSpec extends AnyWordSpec{
   private val map = TileMap()
   "A tile map" should {
     "be able to construct a map that maps all indexes to empty values" in {
-      assert(map.data.isInstanceOf[SortedMap[(Index, Index), Option[Card]]])
+      assert(map.data.isInstanceOf[SortedMap[(Index, Index), Option[Tile]]])
       assert(map.data(Index(0), Index(0)).isEmpty)
     }
   }
