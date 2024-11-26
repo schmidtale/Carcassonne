77b3042fd5e17eddca040112015166db7e19f6db
schmidtale <schmidt.trossingen@web.de>
2024-11-26 15:30:00 +0100

    Commented unfinished addCommand code out

--- src/main/scala/controller/addCommand.scala
+++ src/main/scala/controller/addCommand.scala
@@ -6,20 +6,23 @@ class addCommand(index1: Index, index2: Index, tile: Tile, tabletop: Tabletop) e
   //var memento: GameState = tabletop.gameState
   override def doStep(): Unit = {
     //memento = tabletop.gameState
-    tabletop.tileMap = tabletop.tileMap.add(index1, index2, Some(tile))
+    
+    //tabletop.tileMap = tabletop.tileMap.add(index1, index2, Some(tile))
   }
 
   override def undoStep(): Unit = {
     //val new_memento = controller.gameState
     //controller.gameState = memento
     //memento = new_memento
-    tabletop.tileMap = tabletop.tileMap.add(index1, index2, Option.empty[Tile])
+    
+    //tabletop.tileMap = tabletop.tileMap.add(index1, index2, Option.empty[Tile])
   }
 
   override def redoStep(): Unit = {
     //val new_memento = controller.gameState
     //controller.gameState = memento
     //memento = new_memento
-    tabletop.tileMap = tabletop.tileMap.add(index1, index2, Some(tile))
+    
+    //tabletop.tileMap = tabletop.tileMap.add(index1, index2, Some(tile))
   }
 }
