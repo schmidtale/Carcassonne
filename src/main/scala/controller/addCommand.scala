33f6bce48e1b92c45f75a2193852d8bd034a4d11
schmidtale <schmidt.trossingen@web.de>
2024-11-26 18:12:34 +0100

    Refactored addCommand to use gameState.

--- src/main/scala/controller/addCommand.scala
+++ src/main/scala/controller/addCommand.scala
@@ -3,26 +3,23 @@ import model.{GameState, Index, Tile}
 import util.Command
 
 class addCommand(index1: Index, index2: Index, tile: Tile, tabletop: Tabletop) extends Command {
-  //var memento: GameState = tabletop.gameState
+  var memento: GameState = tabletop.gameState
   override def doStep(): Unit = {
-    //memento = tabletop.gameState
-    
-    //tabletop.tileMap = tabletop.tileMap.add(index1, index2, Some(tile))
+    memento = tabletop.gameState
+
+    val newMap = tabletop.gameState.map.add(index1, index2, Some(tile))
+    tabletop.gameState = tabletop.gameState.withMap(newMap)
   }
 
   override def undoStep(): Unit = {
-    //val new_memento = controller.gameState
-    //controller.gameState = memento
-    //memento = new_memento
-    
-    //tabletop.tileMap = tabletop.tileMap.add(index1, index2, Option.empty[Tile])
+    val new_memento = tabletop.gameState
+    tabletop.gameState = memento
+    memento = new_memento
   }
 
   override def redoStep(): Unit = {
-    //val new_memento = controller.gameState
-    //controller.gameState = memento
-    //memento = new_memento
-    
-    //tabletop.tileMap = tabletop.tileMap.add(index1, index2, Some(tile))
+    val new_memento = tabletop.gameState
+    tabletop.gameState = memento
+    memento = new_memento
   }
 }
